1d68cc9a69b60a097028082c733ed2d42f16a258
SubhajitManna-B <subhajitmanna2002@gmail.com>
2025-02-15 22:45:09 +0530

    Add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import express from 'express'
+
+const app = express()
 
 dotenv.config({
     path: './env'
@@ -8,7 +11,15 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed: ", error);
+});
 
 
 
