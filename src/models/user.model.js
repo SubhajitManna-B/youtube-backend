eab87826a1e591d26a4a71fe5dd2cb7b86b05c35
SubhajitManna-B <subhajitmanna2002@gmail.com>
2025-04-21 23:59:05 +0530

    Add user password change, current user retrieval, and account update functionality; implement subscription model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -45,9 +45,9 @@ const userSchema = new Schema({
 }, {timestamps: true})
 
 userSchema.pre("save", async function(next){
-    if(this.isModified("password")){
-        this.password = await bcrypt.hashSync(this.password, 10)
-    }
+    if(!this.isModified("password")) return next();
+    
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
